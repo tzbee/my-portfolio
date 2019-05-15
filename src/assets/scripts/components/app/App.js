9f385b745efe52af1bc7d3c709d082731d3c9c2f
Amine Tolba <tolbimine@gmail.com>
2019-05-15 15:38:42 +0300

    Routing pages

--- src/assets/scripts/components/app/App.js
+++ src/assets/scripts/components/app/App.js
@@ -4,25 +4,52 @@ import React from 'react';
 import AboutMe from './AboutMe';
 import MyProjects from './MyProjects';
 
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
 const App = () => {
 	return (
-		<div className="App">
-			<div className="App-content">
-				<div className="App-menu">
-					<div className="menu">
-						<div className="menu-items">
-							<div className="menu-item">ME</div>
-							<div className="menu-item">PROJECTS</div>
-							<div className="menu-item">CONTACT</div>
+		<Router>
+			<div className="App">
+				<div className="App-content">
+					<div className="App-menu">
+						<div className="menu">
+							<div className="menu-items">
+								<Link to="/" className="menu-item">
+									ME
+								</Link>
+								<Link to="/projects" className="menu-item">
+									PROJECTS
+								</Link>
+								<Link to="/contact" className="menu-item">
+									CONTACT
+								</Link>
+							</div>
 						</div>
 					</div>
+					<Route
+						path="/"
+						exact
+						component={() => (
+							<div className="App-aboutMe">
+								<AboutMe />
+							</div>
+						)}
+					/>
+					<Route
+						path="/projects"
+						component={() => (
+							<div className="App-projects">
+								<MyProjects />
+							</div>
+						)}
+					/>
+					<Route
+						path="/contact"
+						component={() => <div className="App-contact" />}
+					/>
 				</div>
-				<div className="App-projects">
-					<MyProjects />
-				</div>
-				<div className="App-aboutMe">{/* <AboutMe /> */}</div>
 			</div>
-		</div>
+		</Router>
 	);
 };
 

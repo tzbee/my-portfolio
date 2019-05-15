97d3dde1c621b7e5383c10c89f019aa0b3271e60
Amine Tolba <tolbimine@gmail.com>
2019-05-15 18:18:53 +0300

    Better alignment for buttons

--- src/assets/scripts/components/app/AboutMe.js
+++ src/assets/scripts/components/app/AboutMe.js
@@ -3,6 +3,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import { Link } from 'react-router-dom';
+
 const AboutMe = ({ className = '' }) => {
 	return (
 		<div className={`AboutMe ${className}`}>
@@ -13,14 +15,15 @@ const AboutMe = ({ className = '' }) => {
 				<strong>full stack web developer</strong>.
 			</p>
 			<div className="AboutMe-footer">
-				<div className="AboutMe-btn-wrp">
-					<button className="AboutMe-btn">Check my stuff</button>
-				</div>
-				<div className="AboutMe-btn-wrp">
-					<button className="AboutMe-btn AboutMe-hire-btn">
-						Hire me
-					</button>
-				</div>
+				<Link
+					to="/projects"
+					className="AboutMe-btn AboutMe-checkStuff-btn"
+				>
+					Check my stuff
+				</Link>
+				<button className="AboutMe-btn AboutMe-hire-btn">
+					Hire me
+				</button>
 			</div>
 		</div>
 	);

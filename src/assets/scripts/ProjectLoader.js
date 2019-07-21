12a1f740cd61209fbf7c61b4b6d0e04242060f9a
tzbee <aminetolba29@gmail.com>
2019-07-21 12:45:15 +0300

    Refactor: cleanup

--- src/assets/scripts/ProjectLoader.js
+++ src/assets/scripts/ProjectLoader.js
@@ -2,8 +2,6 @@
 
 import axios from "axios";
 
-const projects = require("../../../resources/projects.json");
-
 const apiURL = "https://portfolio-with-service.herokuapp.com/api/projects";
 
 export default class ProjectLoader {
